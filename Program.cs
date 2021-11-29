1882116cab87d7390e8eb3b6fb82bf381ce02fba
kmarouzi <kian.maroozi@gmail.com>
2021-11-29 18:00:36 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace hello_kian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.Write("what's your name? "); 
+        string name = Console.ReadLine(); 
+        Console.WriteLine("Hello " + name); // Hello name //
+            Console.Write("what's your birth year? ");
+         string year = Console.ReadLine();
+            int age = 2021 - int.Parse(year);
+            Console.WriteLine("your age: " + age);
+            Console.ReadKey();
+        }
+    }
+}
